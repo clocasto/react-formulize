4ee1bfaa3009677f682d1f1408cc70acd677f057
Chris LoCasto <locasto1@gmail.com>
2017-02-09 17:01:32 -0600

    no longer passing all props to input element - instead added support for onFocus and onBlur props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 const Input = props => (
   <label htmlFor={props.name}>
     <input
-      {...props}
       checked={props.checked}
       type={props.type}
       name={props.name}
       value={props.value}
       onChange={props.onChange}
+      onBlur={props.onBlur}
+      onFocus={props.onFocus}
     />
   </label>
 );
@@ -16,6 +17,8 @@ const Input = props => (
 Input.propTypes = {
   name: React.PropTypes.string,
   onChange: React.PropTypes.func,
+  onFocus: React.PropTypes.func,
+  onBlur: React.PropTypes.func,
   type: React.PropTypes.string,
   value: React.PropTypes.oneOfType([
     React.PropTypes.string,
@@ -28,6 +31,8 @@ Input.propTypes = {
 Input.defaultProps = {
   name: null,
   onChange: null,
+  onFocus: null,
+  onBlur: null,
   type: 'text',
   value: '',
   checked: null,
